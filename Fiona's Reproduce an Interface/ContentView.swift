e8a23cfb33dacfb7df5731ce02808c40db3fc944
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-06 14:23:37 -0500

    Initial Commit

---
+++ Fiona's Reproduce an Interface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fiona's Reproduce an Interface
+//
+//  Created by Fiona ZHOU on 2023-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fiona's Reproduce an Interface/Fiona_s_Reproduce_an_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fiona_s_Reproduce_an_InterfaceApp.swift
+//  Fiona's Reproduce an Interface
+//
+//  Created by Fiona ZHOU on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct Fiona_s_Reproduce_an_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
