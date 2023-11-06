53391b5e96229c6af2eb74b5d143949af195cb04
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-06 15:41:39 -0500

    Completed a swiftUI sub-body supporting my main contentView so that I would not repeat.

---
+++ Fiona's Reproduce an Interface/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Fiona's Reproduce an Interface
+//
+//  Created by Fiona ZHOU on 2023-11-06.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
