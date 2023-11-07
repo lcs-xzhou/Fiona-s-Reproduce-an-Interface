e52590b73f2c986133438a7a9d7a539934d9f561
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-06 19:10:15 -0500

    Created lists of item and adjust the color of it.

--- Fiona's Reproduce an Interface/ContentView.swift
+++ Fiona's Reproduce an Interface/ContentView.swift
@@ -9,16 +9,118 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            HStack {
+                
+            }
+            List {
+                Group {
+                    SwiftUIView(
+                        title1: "Mars Exploration Inc.",
+                        title2: "Explorer App",
+                        subTitle: "Design",
+                        content: "Designing onboarding",
+                        time: "01:37:42",
+                        rectangleColor: .white,
+                        circleColor1: .white,
+                        circleColor2: Color("Blue-1"),
+                        rectangleColor1: .white,
+                        textColor: Color("Blue-1")
+                    )
+                    .listRowBackground(Color("Blue-1"))
+                    SwiftUIView(
+                        title1: "Mars Exploration Inc.",
+                        title2: "Lightweight Pizza App",
+                        subTitle: "ios Development",
+                        content: "Started implementing AR gravity simulator",
+                        time: "01:07:37",
+                        rectangleColor: Color("Orange-1"),
+                        circleColor1: Color("Orange-1"),
+                        circleColor2: Color("Orange-1"),
+                        rectangleColor1: Color("Orange-1"),
+                        textColor: .white
+                    )
+                    .listRowBackground(Color("Orange-1"))
+                    SwiftUIView(
+                        title1: "Wildlife Finder Inc.",
+                        title2: "Bear With Me",
+                        subTitle: "ios Development",
+                        content: "Continued implementing explore view",
+                        time: "03:15:00",
+                        rectangleColor: Color("Black-1"),
+                        circleColor1: Color("Black-1"),
+                        circleColor2: Color("Black-1"),
+                        rectangleColor1: Color("Black-1"),
+                        textColor: .white
+                    )
+                    .listRowBackground(Color("Black-1"))
+                    SwiftUIView(
+                        title1: "Mars Exploration Inc.",
+                        title2: "Bear With Me",
+                        subTitle: "ios Development",
+                        content: "Continued implementing explore view",
+                        time: "02:13:00",
+                        rectangleColor: Color("Purple-3"),
+                        circleColor1: Color("Purple-3"),
+                        circleColor2: Color("Purple-3"),
+                        rectangleColor1: Color("Purple-3"),
+                        textColor: .white
+                    )
+                    .listRowBackground(Color("Purple-3"))
+                }
+                .padding()
+                .listRowInsets(
+                    .init(
+                        top: 0,
+                        leading: 0,
+                        bottom: 0,
+                        trailing: 0
+                    )
+                )
+
+            }
+            .listStyle(.sidebar)
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    TabView(selection: Binding.constant(5)) {
+        
+        ContentView()
+            .tabItem {
+                Image(systemName: "timer")
+                Text("Time Entries")
+            }
+        
+        Text("Invoices")
+            .tabItem {
+                Image(systemName: "doc.fill")
+                Text("Invoices")
+            }
+        
+        Text("Dashboard")
+            .tabItem {
+                Image(systemName: "gauge.with.dots.needle.bottom.50percent")
+                Text("Dashboard")
+            }
+        
+        Text("Clients")
+            .tabItem {
+                Image(systemName: "briefcase")
+                Text("Clients")
+            }
+        
+        Text("Company")
+            .tabItem {
+                Image(systemName: "person.2.crop.square.stack")
+                Text("Company")
+            }
+    }
+    // Change the accent color for the currently active tab item
+    .accentColor(Color("Purple-1"))
+    // Ensure tab items that are not active remain visible
+    .preferredColorScheme(.dark)
+    
 }
--- Fiona's Reproduce an Interface/SwiftUIView.swift
+++ Fiona's Reproduce an Interface/SwiftUIView.swift
@@ -8,11 +8,95 @@
 import SwiftUI
 
 struct SwiftUIView: View {
+    
+    let title1: String
+    let title2: String
+    let subTitle: String
+    let content: String
+    let time: String
+    let rectangleColor: Color
+    let circleColor1: Color
+    let circleColor2: Color
+    let rectangleColor1: Color
+    let textColor: Color
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            HStack {
+                VStack {
+                    HStack {
+                        Text(title1)
+                            .font(.title2)
+                        Spacer()
+                    }
+                    HStack {
+                        Text(title2)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Spacer()
+                    }
+                    HStack {
+                        Text(subTitle)
+                            .font(.body)
+                        Spacer()
+                    }
+                    HStack {
+                        Text(content)
+                            .font(.footnote)
+                        Spacer()
+                    }
+                }
+                VStack {
+                    HStack {
+                        Spacer()
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(rectangleColor)
+                                .frame(width: 70, height: 20)
+                                .padding()
+                            Text(time)
+                                .font(.body)
+                                .foregroundColor(textColor)
+                        }
+                    }
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Spacer()
+                        ZStack {
+                            Circle()
+                                .foregroundColor(circleColor1)
+                                .frame(width: 25)
+                            Circle()
+                                    .foregroundColor(circleColor2)
+                                    .frame(width: 23)
+                            Rectangle()
+                                    .foregroundColor(rectangleColor1)
+                                    .frame(width: 8, height: 8)
+                                    .scaledToFit()
+                        }
+                        Spacer()
+                    }
+                }
+
+            }
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    SwiftUIView(
+        title1: "Mars Exploration Inc.",
+        title2: "Explorer App",
+        subTitle: "Design",
+        content: "Designing onboarding",
+        time: "01:37:42",
+        rectangleColor: .white,
+        circleColor1: .white,
+        circleColor2: Color("Blue-1"),
+        rectangleColor1: .white,
+        textColor: Color("Blue-1")
+    )
 }
